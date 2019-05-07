b915eb145ba1b5b4342a0aae1f7c2c637c4a2eb8
liusonghuang <huangliusong1994@gmail.com>
2019-05-07 15:00:14 +0800

    指针指向函数

--- hls-lesson8/str_arr_p.c
+++ hls-lesson8/str_arr_p.c
@@ -13,6 +13,8 @@ void T2(){
     printf("\nT2\n");
     void test11();
     test11();
+    void test22();
+    test22();
 }
 
 void test11(){
@@ -20,4 +22,48 @@ void test11(){
     char *a="I love China!";
     a=a+7;
     printf("%s\n",a);
+}
+
+void test22(){
+    int max(int x,int y);
+    int min(int x,int y);
+    int (*p)(int,int);
+    int a,b,c,n;
+    printf("please enter a and b:");
+    scanf("%d,%d",&a,&b);
+    printf("please choose 1 or 2:");
+    scanf("%d",&n);
+    if (n==1) {
+        p=max;
+    }else if(n==2){
+        p=min;
+    }
+    c=(*p)(a,b);
+    printf("a=%d,b=%d\n",a,b);
+    if (n==1) {
+        printf("max=%d\n",c);
+    }else if(n==2){
+        printf("min=%d\n",c);
+    }
+}
+
+
+int max(int x,int y){
+    int z;
+    if (x>y) {
+        z=x;
+    }else{
+        z=y;
+    }
+    return z;
+}
+
+int min(int x,int y){
+    int z;
+    if (x<y) {
+        z=x;
+    }else{
+        z=y;
+    }
+    return z;
 }
