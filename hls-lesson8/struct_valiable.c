a97a8e724284f2de8339178a3f7f098bad70f080
liusonghuang <huangliusong1994@gmail.com>
2019-05-07 16:54:56 +0800

    结构体 赋值 输出

--- hls-lesson8/struct_valiable.c
+++ hls-lesson8/struct_valiable.c
@@ -10,8 +10,15 @@
 
 void struct_test1(){
     //printf("\nstruct_test1\n");
-    void s_testt(void);
-    s_testt();
+    //void s_testt(void);
+    //s_testt();
+    
+    //void s_testt1();
+    //s_testt1();
+    
+    //结构体数组
+    void s_testt2();
+    s_testt2();
 }
 
 
@@ -23,4 +30,27 @@ void s_testt(void){
         char addr[20];
     }a={10101,"huangliusong",'M',"123 beijing road"};
     printf(" No:%ld\n name:%s\n sex:%c\n addr:%s\n",a.num,a.name,a.sex,a.addr);
+}
+
+void s_testt1(){
+    struct Student{
+        int num;
+        char name[20];
+        float score;
+    }student1,student2;
+    scanf("%d%s%f",&student1.num,&student1.name,&student1.score);
+    scanf("%d%s%f",&student2.num,&student2.name,&student2.score);
+    printf("the higher score is:\n");
+    if (student1.score>student2.score) {
+        printf("%d %s %6.2f\n",student1.num,student1.name,student1.score);
+    }else if (student1.score<student2.score){
+        printf("%d %s %6.2f\n",student2.num,student2.name,student2.score);
+    }else{
+        printf("%d %s %6.2f\n",student1.num,student1.name,student1.score);
+        printf("%d %s %6.2f\n",student2.num,student2.name,student2.score);
+    }
+}
+
+void s_testt2(){
+    
 }
